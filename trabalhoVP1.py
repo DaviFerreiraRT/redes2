646e17ad491f7238fc68715c15f4d8a614742f00
Davi Ferreira <daviferreirart@gmail.com>
2021-09-14 20:11:39 -0300

    implementando logica de verificação ip

--- trabalhoVP1.py
+++ trabalhoVP1.py
@@ -2,21 +2,23 @@ import csv
 ip=[]
 mask = []
 interface=[]
-with open('tabelaRota.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        ip.append(row[0])
-        mask.append(row[1])
-        interface.append(row[2])
+def saida(ipCliente,arquivo):
+    with open(arquivo, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            ip.append(row[0])
+            mask.append(row[1])
+            interface.append(row[2])
 
+            ipFormatado = ip[1:]
+            maskFormatada = mask[1:]
+            interfaceFormatada = interface[1:]
 
-ipFormatado = ip[1:]
-maskFormatada = mask[1:]
-interfaceFormatada = interface[1:]
+        if (ipCliente in (ip)):
+            print("O IP",ipCliente,"está na tabela de rotas")
+        else:
+            print("O IP",ipCliente,"não está alocado na tabela")
 
-a = ipFormatado[0]
-
-primeiraParte = a[0:a.index(".")]
-
-print(bin(int(primeiraParte))[2:])
+saida("125.102.32.108","tabelaRota.csv")
+    
 
